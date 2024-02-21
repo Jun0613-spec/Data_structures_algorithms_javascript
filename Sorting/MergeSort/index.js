40dcde04e03c1c1f0e2682909f60457efda9bc40
Jun0613-spec <kumo5110@gmail.com>
2024-02-21 14:49:22 +0000

    MergeSort

--- Sorting/MergeSort/index.js
+++ Sorting/MergeSort/index.js
@@ -2,9 +2,9 @@
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-//O(log N)
+//O(n log n)
 const mergeSort = (array) => {
-  if (array.length === 1) {
+  if (array.length <= 1) {
     return array;
   }
   // Split Array in into right and left
